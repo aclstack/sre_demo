acceee870c477ef51b4865734dcfc946ef59bbcd
aclstack <aclstack@gmail.com>
2019-04-03 15:14:59 +0800

    我也不知道改了啥

--- app.py
+++ app.py
@@ -5,7 +5,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    items = (['张三', 'zs'], ['李四', 'ls'], ['王五', 'ww'])
+    return render_template('index.html', items=items)
+
+
+@app.route('/welcome.html')
+def welcome():
+    return render_template('welcome.html')
 
 
 if __name__ == '__main__':
