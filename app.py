9782fab834376af14f17ec0b44a16efe315d85b1
aclstack <aclstack@gmail.com>
2019-04-03 19:18:17 +0800

    add login

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -9,10 +9,19 @@ def hello_world():
     return render_template('index.html', items=items)
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        return redirect('http://127.0.0.1:8000', code=302)
+
+
+
 @app.route('/welcome.html')
 def welcome():
     return render_template('welcome.html')
 
 
 if __name__ == '__main__':
-    app.run(port=8000)
+    app.run(port=8000, debug=True)
